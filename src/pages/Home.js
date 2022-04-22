a34c13b45b32dca8543d6d02f055d5dabaefe173
stoneboat <tood2111@gmail.com>
2022-04-22 14:52:50 +0900

    라우팅 설정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import PangImage from "../assets/cat1.jpg";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleClickButton = () => {
+    navigate("/question");
+  };
+
   return (
     <Wrapper>
       <Header>예비집사판별기</Header>
@@ -18,7 +25,12 @@ const Home = () => {
           />
         </LogoImage>
         <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기</Desc>
-        <Button>테스트 시작하기</Button>
+        <Button
+          style={{ fontFamily: "SimKyungha" }}
+          onClick={handleClickButton}
+        >
+          테스트 시작하기
+        </Button>
       </Contents>
     </Wrapper>
   );
@@ -36,6 +48,7 @@ const Header = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  font-family: "SimKyungha";
 `;
 
 const Contents = styled.div`
@@ -48,6 +61,7 @@ const Contents = styled.div`
 const Title = styled.div`
   font-size: 30px;
   margin-top: 40px;
+  font-family: "SimKyungha";
 `;
 
 const LogoImage = styled.div`
@@ -57,4 +71,5 @@ const LogoImage = styled.div`
 const Desc = styled.div`
   font-size: 20px;
   margin-top: 20px;
+  font-family: "SimKyungha";
 `;
