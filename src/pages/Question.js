3e3bcda04d0b127fabffaa5fe87454c4e15e4eac
stoneboat <tood2111@gmail.com>
2022-04-22 19:34:56 +0900

    add question page

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,7 +1,52 @@
 import React from "react";
+import styled from "styled-components";
+import { ProgressBar, Button } from "react-bootstrap";
+import { QuestionData } from "../assets/data/questiondata";
 
 const Question = () => {
-  return <div>Question</div>;
+  return (
+    <Wrapper>
+      <ProgressBar
+        striped
+        variant='danger'
+        now={80}
+        style={{ marginTop: "20px" }}
+      />
+      <Title>{QuestionData[0].title}</Title>
+      <ButtonGroup>
+        <Button style={{ width: "40%", minHeight: "200px", fontSize: "15pt" }}>
+          {QuestionData[0].answera}
+        </Button>
+        <Button
+          style={{
+            width: "40%",
+            minHeight: "200px",
+            fontSize: "15pt",
+            marginLeft: "20px",
+          }}
+        >
+          {QuestionData[0].answerb}
+        </Button>
+      </ButtonGroup>
+    </Wrapper>
+  );
 };
 
 export default Question;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+  text-align: center;
+  font-family: "SimKyungha";
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  font-family: "SimKyungha";
+`;
